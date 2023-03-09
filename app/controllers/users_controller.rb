d51bbe10db6e8e354525885576a2c85021c7ee99
Rishaab Kalra <55053464+PeaPals@users.noreply.github.com>
2023-03-09 10:33:44 +0530

    Added user profile edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # before_action :get_user, only: [:show, :update, :edit, :destroy]
+  before_action :get_user, only: [:edit, :update]
 
 
   def new
@@ -7,7 +9,39 @@ class UsersController < ApplicationController
 
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, sign-up successful."
+      redirect_to articles_path
+    else
+      render 'new'
+    end
   end
+
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile updated successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+
+
+  private
+
+  def get_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 
 end
