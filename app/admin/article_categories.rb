0422c9025317106b707f537b29cc3cfb377d0dac
Rishaab Kalra <55053464+PeaPals@users.noreply.github.com>
2023-03-29 15:24:51 +0530

    minor changes

--- app/admin/article_categories.rb
+++ app/admin/article_categories.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register Article do
+ActiveAdmin.register ArticleCategory do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :description, :user_id, category_ids: []
+  permit_params :article_id, :category_id
   #
   # or
   #
   # permit_params do
-  #   permitted = [:title, :description, :UUID, :user_id]
+  #   permitted = [:article_id, :category_id]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
