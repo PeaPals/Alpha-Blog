f7211da55684a972f8c5163e47b398cf8ff25f5a
Rishaab Kalra <55053464+PeaPals@users.noreply.github.com>
2023-04-01 10:23:03 +0530

    added search functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,29 @@
 class PagesController < ApplicationController
 
   def home
+    searchphrase = params[:searchphrase]
+
+    if searchphrase && searchphrase != ""
+      searchphrase = searchphrase.strip.split(' ')
+      @searched_results = search_results(searchphrase)
+    end
+
     render "home"
   end
+
+
+  def search_results(searchphrase)
+    searched_results = {:users => [], :articles => [], :categories => []}
+
+    searchphrase.each do |word|
+      searched_results[:users] += User.where("username like ?", "%#{word}%")
+      searched_results[:articles] += Article.where("title like ?", "%#{word}%")
+      searched_results[:categories] += Category.where("name like ?", "%#{word}%")
+    end
+
+    return searched_results
+  end
+
+
 
 end
